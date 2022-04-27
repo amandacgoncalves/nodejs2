1e33ced9c98cf0141ccdb604a44797eee73b16cb
Amanda <amandacgoncalves2002@gmail.com>
2022-04-27 12:32:06 -0300

    starting postman

--- routes/users.js
+++ routes/users.js
@@ -17,14 +17,9 @@ app.get('/users', (req, res)=> {
 
 });
 
-app.get('/users/admin', (req, res)=>{
+app.post('/users', (req, res)=>{
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users:[]
-
-    });
+    res.json(req.body);
 
 });
 
